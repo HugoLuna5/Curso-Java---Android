f53b362210027c04d0ba9c0509e1fc0ceac4a104
Hugo Dario Luna Cruz <hugo_1199@hotmail.com>
2017-10-02 09:09:36 -0500

    Create Actividad01.java

---
+++ Introduccion a la Programacion/Actividad01.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursointrojava;
+
+
+/**
+ *
+ * @author HugoLuna
+ */
+public class Actividad01 {
+    
+    public static void main(String[] args) {
+        
+        
+        //declarando variables
+        int a= 10,b= 20, res;
+        
+        
+        /*Impresion de valores por consola*/
+        
+        System.out.println("Variable de tipo entero a = "+a+"\n Variable de tipo entero b ="+b);
+        
+        
+        /* Suma de dos variables*/
+        res = a+b;
+        System.out.println("La suma de "+a+" + "+b+" es igual a "+res);
+        
+        /* Resta de dos variables*/
+        res = a-b;
+        System.out.println("La resta de "+a+" - "+b+" es igual a "+res);
+        
+        /* Suma de dos variables*/
+        res = a*b;
+        System.out.println("La multiplicación de "+a+" * "+b+" es igual a "+res);
+        
+        /* Division de dos variables*/
+        res = a/b;
+        System.out.println("La división de "+a+" / "+b+" es igual a "+res);
+        
+    }
+}
