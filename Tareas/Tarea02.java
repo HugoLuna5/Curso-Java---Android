45cfa1406c891d47908dda7b7747b1c3b6e57bf1
Hugo Dario Luna Cruz <hugo_1199@hotmail.com>
2017-10-15 13:38:20 -0500

    Create Tarea02.java

---
+++ Tareas/Tarea02.java
@@ -0,0 +1,15 @@
+package tareascurso;
+
+/**
+ *
+ * @author HugoLuna
+ */
+public class Tarea02 {
+    /*
+    Escribe un programa que calcule el salario semanal
+    de un empleado en base a las horas trabajadas, a razón de 50 pesos la hora.
+    Ejemplo. 
+    Horas trabajadas            Salario semanal
+        10                             500 
+    */
+}
