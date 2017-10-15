38e342341d83af2bdacc5ddb7f5e0ebd65681ffd
Hugo Dario Luna Cruz <hugo_1199@hotmail.com>
2017-10-15 13:32:31 -0500

    Create Tarea01.java

---
+++ Tareas/Tarea01.java
@@ -0,0 +1,16 @@
+package tareascurso;
+
+/**
+ *
+ * @author HugoLuna
+ */
+public class Tarea01 {
+    /*
+    Calcular el promedio de un alumno y mostrar un mensaje segun su calificación
+    segun lo correspondiente
+    (cal < 7) Reprobado
+    (cal==7) = Aprobado, por poco, 
+    (cal==8 || cal ==9) = Muy bien, aprobado
+    (cal ==10) Muy bien, felicidades
+    */
+}
