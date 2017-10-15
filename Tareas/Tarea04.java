255b20eee84ffa77528219179a9fceba2a5f3ca5
Hugo Dario Luna Cruz <hugo_1199@hotmail.com>
2017-10-15 13:43:28 -0500

    Create Tarea04.java

---
+++ Tareas/Tarea04.java
@@ -0,0 +1,12 @@
+package tareascurso;
+
+/**
+ *
+ * @author HugoLuna
+ */
+public class Tarea04 {
+    /*
+    *Escribe un programa que pida por teclado un día de la semana
+    *y que diga qué asignatura toca a primera hora ese día.
+    */
+}
