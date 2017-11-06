25c941785b810eef1982913952e4d7a2ae358a2f
Hugo Dario Luna Cruz <hugo_1199@hotmail.com>
2017-11-06 09:19:39 -0600

    Create Actividad07.java

---
+++ Introduccion a la Programacion/Actividad07.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursointrojava;
+
+/**
+ *
+ * @author HugoLuna
+ */
+public class Actividad07 {
+    /*Escribe un programa que imprima del 1 al 10 usando el ciclo for*/
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);        
+        }
+    }
+}
