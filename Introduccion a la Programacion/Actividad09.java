0d0dd72decfc95e29cf96a7c8fc47f217a3086e8
Hugo Dario Luna Cruz <hugo_1199@hotmail.com>
2017-11-06 09:22:10 -0600

    Create Actividad09.java

---
+++ Introduccion a la Programacion/Actividad09.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursointrojava;
+
+/**
+ *
+ * @author HugoLuna
+ */
+public class Actividad09 {
+    /*Escribe un programa que imprima del 1 al 10 usando el ciclo do while*/
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 11);
+    }
+}
